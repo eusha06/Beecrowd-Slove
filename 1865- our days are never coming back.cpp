4e534bea3aa0b414e36a9200acd97f018f43bfd0
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2023-12-04 00:13:33 +0600

    Create 1865- our days are never coming back.cpp

---
+++ 1865- our days are never coming back.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sakalakabumbum() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long int
+#define pb push_back
+#define forn(i, n) for(int i = 0; i < int(n); i++)
+#define MOD 1000000007
+#define w(t) int t; cin >> t; cin.ignore(); while (t--) // Added cin.ignore()
+int main() {
+    sakalakabumbum();
+    string a="LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int i,n;
+    cin>>n;
+    forn(i,n){
+    	cout<<a[i];
+    }
+    cout<<endl;
+
+    return 0;
+}
