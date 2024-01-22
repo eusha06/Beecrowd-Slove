7dd6a9a115c3ecfaaf393b9c0ad6c4d01caafe64
Eusha Hasan <113366884+eusha06@users.noreply.github.com>
2024-01-22 18:06:59 +0600

    Bee - 1070 - Six Odd Numbers.c

---
+++ Bee - 1070 - Six Odd Numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+       int n;
+  scanf("%d", &n);
+  for
+  (int i=n; i<=n+11; i++)
+  {
+      if
+      (i%2!=0)
+      {
+          printf("%d\n",i);
+      }
+  }
+    return 0;
+}
